3d760660d4ad70b38abbbdac0196a2302bf2f072
niklas <niklasjsson@gmail.com>
2018-09-09 17:15:24 +0200

    added breakpoints and refactor gallery

---
+++ frontend/src/components/gallery/Gallery.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="gallery" :class="vm.$mq">
+    <div v-for="(image, $index) in vm.images"
+          :key="$index"
+          class="gallery-image">
+      <img :alt="image.name"
+            :src="image.src"
+            :title="image.description" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['images'],
+  computed: {
+    vm () {
+      return this
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.gallery {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 380px;
+
+  .gallery-image {
+    display: flex;
+    padding: 0px 8px;
+    border-radius: 4px;
+    margin: 0px 20px;
+    justify-content: center;
+    align-items: center;
+    cursor: cell;
+
+    &:hover {
+      img {
+        transform: scale(1.1);
+        box-shadow: 0px 0px 30px 0px rgba(50, 50, 50, 0.7);
+      }
+    }
+
+    img {
+      border-radius: 2px;
+      height: 300px;
+      transition-duration: 1.1s;
+    }
+  }
+}
+</style>
--- frontend/src/modules/architecture/Architecture.vue
+++ frontend/src/modules/architecture/Architecture.vue
@@ -1,15 +1,7 @@
 <template>
   <div class="about">
     <h1 class="header">Architecture</h1>
-    <div class="gallery">
-      <div v-for="(image, $index) in vm.images"
-           :key="$index"
-           class="gallery-image">
-        <img :alt="image.name"
-             :src="image.src"
-             :title="image.description" />
-      </div>
-    </div>
+    <Gallery :images="vm.images"/>
     <div class="content">
       <div>
         <p>
@@ -44,10 +36,13 @@
 </template>
 
 <script>
+import Gallery from '@/components/gallery/Gallery'
+
 import imgArchDiscovery from '@/assets/arcitecture_discovery.jpg'
 import imgArchContainers from '@/assets/architecture_containers.jpg'
 
 export default {
+  components: { Gallery },
   data () {
     return {
       images: [
@@ -77,35 +72,6 @@ export default {
   cursor: default;
   user-select: none;
 
-  .gallery {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 380px;
-
-    .gallery-image {
-      display: flex;
-      padding: 0px 8px;
-      border-radius: 4px;
-      margin: 0px 20px;
-      justify-content: center;
-      align-items: center;
-      cursor: cell;
-
-      &:hover {
-        img {
-          transform: scale(1.1);
-          box-shadow: 0px 0px 30px 0px rgba(50, 50, 50, 0.7);
-        }
-      }
-
-      img {
-        border-radius: 2px;
-        height: 300px;
-        transition-duration: 1.1s;
-      }
-    }
-  }
   .content {
     width: 800px;
     margin: 0px auto;
